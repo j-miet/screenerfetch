87c335bd19c4a9f37b5c0ea3d9c20ebfcc4561b7
j-miet <191247923+j-miet@users.noreply.github.com>
2025-03-21 19:40:15 +0200

    finalized custom workbook creation tools and commands

--- screenerfetch/custom/c_commands.py
+++ screenerfetch/custom/c_commands.py
@@ -1,12 +1,16 @@
 """Current custom workbook commands."""
 
+import json
+
 import custom.plot as plot
 import custom.c_workbook_tools as c_workbook_tools
+import paths
 from query import QueryVars
-from workbook_tools import WorkSheets, check_date
+import sheets
+import workbook_tools
 
 def _check_wb_validity() -> bool:
-    """Check if workbook supports current custom plotting commands."""
+    """Check if current settings supports custom plotting commands."""
     if ({"Date", 
         "Symbol",
         "Low",
@@ -32,7 +36,7 @@ def _check_wb_validity() -> bool:
     }.issubset(QueryVars.actual_columns)):
         return True
     print("########################################################\n"
-          "### Current workbook doesn't support custom commands ###\n"
+          "### Current settings don't support custom commands ###\n"
           "########################################################\n"
           "Required:\n"
           "<columns> name, low, high, close, open, float_shares_outstanding_current, volume, market_cap_basic, " "premarket_open, premarket_close, change_from_open\n"
@@ -61,7 +65,7 @@ def _plot_data() -> None:
         elif user_input == 'daily cs':
             date_input = input('Enter a date found that corresponds to an existing date in your excel workbook. ' 
                                'Date must in format "YYYY-MM-DD" (with \'-\' as separator!).\n=>')
-            if check_date(date_input):
+            if workbook_tools.check_date(date_input):
                 plot.show_daily_candles(date_input)
         elif user_input == 'dist':
             plot.show_distributions()
@@ -76,7 +80,7 @@ def _add_row_in_sheet2() -> None:
     This second worksheet is meant to store custom information which must be updated either via existing commands or 
     by manually inside workbook.
     """
-    print(f'Type \'exit\' to stop adding new rows in {WorkSheets.SHEET2}.\n'
+    print(f'Type \'exit\' to stop adding new rows in {sheets.WorkbookSheetNames.sheet_names[1]}.\n'
           'Need date and symbol (always in uppercase) to identify correct rows.\n'
           'Format is SYMBOL YYYY-MM-DD . Example: NVDA 2024-01-01 \n'
           '-> '   
@@ -143,31 +147,51 @@ def _add_images() -> None:
         except (IndexError, ValueError):
             print('Invalid input.')
 
+def _update_datetime() -> None:
+    c_workbook_tools.custom_update_datetime()
+
+def _create_custom_wb() -> None:
+    c_workbook_tools.create_custom_wb()
+
 def select_custom_command() -> None:
     """Lists all custom workbook commands and asks user input if workbook format is valid."""
     if _check_wb_validity():
-        while True:
-            custom_input = input("Type any of the custom commands below, or 'back' to return.\n"
-                f"add row = adds a row for existing symbol in the second worksheet {WorkSheets.SHEET2}; has "
-                "custom columns.\n"
-                f"notes = adds/overrides notes for any listed symbol in {WorkSheets.SHEET2} sheet.\n"
-                f"images = add hyperlinks in {WorkSheets.SHEET2} sheet which point to 'custom/images' folder.\n"
-                "plot = commands for visualizing symbol data of current workbook.\n"
-                "FORMAT CUSTOM WB = formats current workbook to custom format, adding a second sheet "
-                f"{WorkSheets.SHEET2}.\n"
-                "[custom]>>> ")
-            if custom_input == 'add row':
-                _add_row_in_sheet2()
-            elif custom_input == 'notes':
-                _add_notes()
-            elif custom_input == 'images':
-                _add_images()
-            elif custom_input == 'plot':
-                _plot_data()
-            elif custom_input == 'back':
-                return
-            elif custom_input == 'FORMAT CUSTOM WB':
-                ... # implement this under c_commands + c_workbook_tools
-                print('This does currently nothing...')
+        with open(paths.FilePaths.SETTINGS_PATH+'\\settings.json') as f:
+            status_check = json.load(f)["status"]
+        if status_check != 'custom':
+            format_input = input("Workbook settings supported. In order to use custom wb, current wb needs to be "
+            "formated to fit the custom layout.\nType 'FORMAT CUSTOM WB' to proceed, or anything else to skip.\n"
+            "-> ")
+            if format_input == 'FORMAT CUSTOM WB':
+                _create_custom_wb()
             else:
-                print('Invalid custom command.')
+                return
+    else:
+        return
+    while True:
+        custom_input = input("Type any of the custom commands below, or 'back' to return.\n"
+            "add row = adds a row for existing symbol in the second worksheet "
+            f"{sheets.WorkbookSheetNames.sheet_names[1]}; has custom columns.\n"
+            "notes = adds/overrides notes for any listed symbol in "
+            f"{sheets.WorkbookSheetNames.sheet_names[1]} sheet.\n"
+            f"images = add hyperlinks in {sheets.WorkbookSheetNames.sheet_names[1]} sheet which point "
+                "to 'custom/images' folder.\n"
+            "plot = commands for visualizing symbol data of current workbook.\n"
+            "FORMAT WB = formats current workbook to custom format.\n"
+            "[custom]>>> ")
+        if custom_input == 'add row':
+            _add_row_in_sheet2()
+        elif custom_input == 'notes':
+            _add_notes()
+        elif custom_input == 'images':
+            _add_images()
+        elif custom_input == 'update date':
+            _update_datetime()
+        elif custom_input == 'plot':
+            _plot_data()
+        elif custom_input == 'FORMAT WB':
+            _create_custom_wb()
+        elif custom_input == 'back':
+            return
+        else:
+            print('Invalid custom command.')
