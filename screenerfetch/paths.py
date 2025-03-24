8234ef91792d084ddbb2b304ec7ecd88f97d0dfe
j-miet <191247923+j-miet@users.noreply.github.com>
2025-03-24 22:32:20 +0200

    removed unused path initializations

--- screenerfetch/paths.py
+++ screenerfetch/paths.py
@@ -3,25 +3,19 @@
 from pathlib import Path
 
 class FilePaths:
-    """File paths and a method to update them.
-
-    Uses pathlib.PATH to find location of this file. 
-    """
+    """File paths and static method upgrade_filepaths to update them."""
     PATH = Path(__file__).parent
-
     WB_FILES_ROOT_PATH = PATH/'workbooks'
 
-    wb_name = ''
-    wb_files_path = WB_FILES_ROOT_PATH/wb_name
-    data_path = wb_files_path/'data'
-    settings_path = wb_files_path/'settings'
-
-    wb_autocopy_name = f'{wb_name} - autocopy'
-    wb_manual_copy_name = f'{wb_name} - copy'
-
-    wb_path = wb_files_path/f'{wb_name}.xlsx'
-    wb_autocopy_path = wb_files_path/f'{wb_autocopy_name}.xlsx'
-    wb_manual_copy_path= wb_files_path/f'{wb_manual_copy_name}.xlsx'
+    wb_name: str
+    wb_files_path: Path
+    data_path: Path
+    settings_path: Path
+    wb_autocopy_name: Path
+    wb_manual_copy_name: Path
+    wb_path: Path
+    wb_autocopy_path: Path
+    wb_manual_copy_path: Path
 
     TXT_NAME = 'api_data'
     TXT_PATH = WB_FILES_ROOT_PATH/f'{TXT_NAME}.txt'
@@ -33,10 +27,9 @@ class FilePaths:
         FilePaths.data_path = FilePaths.wb_files_path/'data'
         FilePaths.settings_path = FilePaths.wb_files_path/'settings'
 
-        FilePaths.wb_autocopy_name = f'{FilePaths.wb_name} - autocopy'
-        FilePaths.wb_manual_copy_name = f'{FilePaths.wb_name} - copy'
+        FilePaths.wb_autocopy_name = f'{FilePaths.wb_name}-autocopy'
+        FilePaths.wb_manual_copy_name = f'{FilePaths.wb_name}-copy'
 
         FilePaths.wb_path = FilePaths.wb_files_path/f'{FilePaths.wb_name}.xlsx'
         FilePaths.wb_autocopy_path = FilePaths.wb_files_path/f'{FilePaths.wb_autocopy_name}.xlsx'
         FilePaths.wb_manual_copy_path= FilePaths.wb_files_path/f'{FilePaths.wb_manual_copy_name}.xlsx'
-        print("File paths updated.")
